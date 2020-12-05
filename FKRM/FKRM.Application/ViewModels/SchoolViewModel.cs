1c78ff108e562caf2df8e5c75da78da037ad4490
Meisam farahani <farahani.m@hotmail.com>
2020-12-05 09:41:11 +0330

    Commit Service and Repository Interfaces,SchoolViewModel

---
+++ FKRM/FKRM.Application/ViewModels/SchoolViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FKRM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FKRM.Application.ViewModels
+{
+    public class SchoolViewModel
+    {
+        public IEnumerable<School> Schools { get; set; }
+    }
+}
