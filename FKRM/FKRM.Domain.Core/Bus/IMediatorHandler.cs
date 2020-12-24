bc8c011be3387110ecd666bc7155c7c3591e353e
Meisam farahani <farahani.m@hotmail.com>
2020-12-24 12:07:10 +0330

    Addimg Domain.Core and implementation of InMemoryBus

---
+++ FKRM/FKRM.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,13 @@
+﻿using FKRM.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FKRM.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command; 
+    }
+}
---
+++ FKRM/FKRM.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using FKRM.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FKRM.Domain.Core.Commands
+{
+    public abstract class Command:Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
