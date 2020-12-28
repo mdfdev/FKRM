5918a73f91e53d88f8fef627d63aa9d4c38dfb52
Meisam farahani <farahani.m@hotmail.com>
2020-12-28 10:35:49 +0330

    Add Api project

---
+++ FKRM/FKRM.Api/Controllers/GenderController.cs
@@ -0,0 +1,28 @@
+﻿using FKRM.Application.Interfaces;
+using FKRM.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FKRM.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenderController : Controller
+    {
+        private readonly IGenderService _genderService;
+
+        public GenderController(IGenderService genderService)
+        {
+            _genderService = genderService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] GenderViewModel genderViewModel)
+        {
+            _genderService.Create(genderViewModel);
+            return Ok(genderViewModel);
+        }
+    }
+}
---
+++ FKRM/FKRM.Api/Startup.cs
@@ -0,0 +1,72 @@
+using FKRM.Infra.Data.Context;
+using FKRM.Infra.IoC;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FKRM.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolDBContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("SchoolDBConnection"));
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c=> {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title="School Api",Version="v1"  });
+            });
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=> {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","School Api v1");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
+        }
+    }
+}
